565d739d6c1d669f7a1d5e6a3dfae29e1c7afe02
Dana <dmarsh@nait.ca>
2024-03-08 11:46:59 -0700

    Mar 8 examples

--- Winter 2024 - Section 1/dino-park-tycoon-starter-01/js/main.js
+++ Winter 2024 - Section 1/dino-park-tycoon-starter-01/js/main.js
@@ -1,8 +1,8 @@
-// TODO Step 1: create an array that represents our dino objects
+// Step 1: create an array that represents our dino objects
+let dinos = [];
 
-
-// TODO Step 2: create HTML element variable for the table body
-
+// Step 2: create HTML element variable for the table body
+const tableBodyElement = document.querySelector('tbody');
 
 const dinoBreeds = [
   "Tyrannosaurus Rex",
@@ -15,38 +15,88 @@ const dinoBreeds = [
   "Deinonychus",
 ].sort();
 
-// TO DO Step 3: populate the drop down list of breeds
+// Step 3: populate the drop down list of breeds
+for(breed of dinoBreeds) { // for each breed in the dinoBreeds array
+  document.querySelector('#dino-species').innerHTML += '<option>' + breed + '</option>';
+}
 
-// HTML syntax:
-// <option>BREED</option>
+// Step 4: create a no-param function called renderTable()
+function renderTable() {
+  tableBodyElement.innerHTML = '';
 
+  dinos.forEach((dino) => {
+    tableBodyElement.innerHTML += `
+     <tr>
+        <td>${dino.name}</td>
+        <td>${dino.species}</td>
+        <td>TO DO: INSERT AGE</td>
+        <td>TO DO: INSERT DIET</td>
+        <td class="remove" data-dino-id="INSERT NUMBER">REMOVE</td>
+      </tr>
+    `;
+    // for age: let's make a helper function that creates today's date = new Date().getFullYear()
+    // for diet: let's make a helper function: carnivore, herbivore, omnivore
+  });
 
-// TO DO Step 4: create a no-param function called renderTable()
-
-// HTML syntax:
-//  <tbody>  
-//   <tr>
-//     <td>INSERT NAME</td>
-//     <td>INSERT SPECIES</td>
-//     <td>INSERT AGE</td>
-//     <td>INSERT DIET</td>
-//     <td class="remove" data-dino-id="INSERT NUMBER">REMOVE</td>
-//   </tr>
-// </tbody> 
-
+} // Dana note to self: fix the starter code template for this HTML
 
 // TO DO Step 5: create a no-param function called renderStats()
+function renderStats(){
+  // calculate # of dinos
+  document.querySelector('#num-dinos').innerText = dinos.length;
 
+  // calculate # of carnivores
+  document.querySelector('#num-carnivores').innerText = countCarnivores();
 
+  // calculate the # of herbivores
 
+  // calculate the # of omnivores
 
+  // calculate the average age
+    // average = total sum / # of dinosaurs
 
-// TO DO Step 6: add an event handler to the form
-// 1. get form values
-// 2. create a new dino with the following properties: id, name, species, birthYear, isMeatEater, and isPlantEater
-// 3. add the new dino object to the array
-// 4. call renderTable()
-// 5. call renderStats()
+}
+
+function countCarnivores() {
+  // iterate through our array, counting the # of carnivores
+  // eats meat but does not eat plants
+  return dinos.filter( dino =>  dino.isMeatEater && !dino.isPlantEater ).length;
+  // could have also used reduce()
+}
+
+const formElement = document.querySelector('#input-form');
+
+// Step 6: add an event handler to the form
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // 1. get form values
+  let dinoName = formElement.elements['dino-name'].value;
+  let dinoSpecies = formElement.elements['dino-species'].value;
+  let dinoYear = formElement.elements['dino-year'].value;
+  let dinoCarnivore = formElement.elements['dino-carnivore'].checked;
+  let dinoHerbivore = formElement.elements['dino-herbivore'].checked;
+
+  // 2. create a new dino with the following properties: id, name, species, birthYear, isMeatEater, and isPlantEater
+  const newDino = {
+    id: dinos.length, 
+    name: dinoName, 
+    species: dinoSpecies, 
+    birthYear: dinoYear, 
+    isMeatEater: dinoCarnivore, 
+    isPlantEater:  dinoHerbivore
+  }
+
+  // 3. add the new dino object to the array
+  dinos.push(newDino);
+
+  // 4. call renderTable()
+  renderTable();
+
+  // 5. call renderStats()
+  renderStats();
+
+});
 
 // CHALLENGE: using a loop, print out all properties & their values to the console
 // CHALLENGE: add input validation: name cannot be empty, species cannot be empty, year cannot be negative or in the future, and a dino must check at least one box
---
+++ dino-park-tycoon-complete/js/main.js
@@ -0,0 +1,182 @@
+// create an array that represents our dino objects
+let dinos = [];
+
+// create HTML element variables
+let tableBodyElement = document.querySelector("tbody");
+
+// populate the drop down list of breeds
+const dinoBreeds = [
+  "Tyrannosaurus Rex",
+  "Triceratops",
+  "Velociraptor",
+  "Stegosaurus",
+  "Archaeopteryx",
+  "Apatosaurus",
+  "Iguanodon",
+  "Deinonychus",
+].sort();
+
+for (breed of dinoBreeds) {
+  document.querySelector(
+    "#dino-species"
+  ).innerHTML += `<option>${breed}</option>`;
+}
+
+// add an event handler to the form to create a new dino object and add it to our array
+let formElement = document.querySelector("#input-form");
+formElement.elements["dino-name"].focus();
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let dinoName = formElement.elements["dino-name"].value;
+  let dinoSpecies = formElement.elements["dino-species"].value;
+  let dinoYear = Number(formElement.elements["dino-year"].value);
+  let eatsMeat = formElement.elements["dino-carnivore"].checked;
+  let eatsPlants = formElement.elements["dino-herbivore"].checked;
+
+  //   check if any fields are empty:
+  if (
+    dinoName.trim().length === 0 ||
+    dinoSpecies.trim().length === 0 ||
+    dinoYear <= 0 ||
+    dinoYear > new Date().getFullYear() ||
+    (!eatsMeat && !eatsPlants)
+  ) {
+    formElement.querySelector(".error").classList.remove("hide");
+  }
+  // otherwise, form is valid:
+  else {
+    formElement.querySelector(".error").classList.add("hide");
+
+    let newDino = {
+      id: dinos.length,
+      name: dinoName,
+      species: dinoSpecies,
+      birthYear: dinoYear,
+      isMeatEater: eatsMeat,
+      isPlantEater: eatsPlants,
+    };
+    dinos.push(newDino);
+
+    // for(property in newDino) {
+    //     console.log(`${property}: ${newDino[property]}`);
+    // }
+
+    renderTable();
+
+    renderStats();
+
+    formElement.reset();
+  }
+});
+
+function renderTable() {
+  tableBodyElement.innerHTML = "";
+
+  let tableSectionElement = document.querySelector("#table-section");
+
+  if (dinos.length > 0) {
+    // show table & hide p
+    tableSectionElement.querySelector("table").classList.remove("hide");
+    tableSectionElement.querySelector("p").classList.add("hide");
+  } else {
+    // hide table & show p
+    tableSectionElement.querySelector("table").classList.add("hide");
+    tableSectionElement.querySelector("p").classList.remove("hide");
+  }
+
+  for (dino of dinos) {
+    tableBodyElement.innerHTML += `  
+            <tr>
+                <td>${dino.name}</td>
+                <td>${dino.species}</td>
+                <td>${getDinoAge(dino.birthYear)}</td>
+                <td>${getDinoDiet(dino)}</td>
+                <td class="remove" data-dino-id="${dino.id}">REMOVE</td>
+            </tr>`;
+  }
+}
+
+function getDinoAge(year) {
+  let today = new Date();
+  let currentYear = today.getFullYear();
+
+  return currentYear - year;
+}
+
+function getDinoDiet(dino) {
+  if (dino.isMeatEater && dino.isPlantEater) {
+    return "omnivore";
+  } else if (dino.isMeatEater) {
+    return "carnivore";
+  } else if (dino.isPlantEater) {
+    return "herbivore";
+  } else {
+    return "unknown";
+  }
+}
+
+function renderStats() {
+  if (dinos.length > 0) {
+    document.querySelector(".dino-stats").classList.remove("hide");
+    document.querySelector(".stats-section p.temp").classList.add("hide");
+  } else {
+    document.querySelector(".dino-stats").classList.add("hide");
+    document.querySelector(".stats-section p.temp").classList.remove("hide");
+  }
+
+  document.querySelector("#num-dinos").innerText = dinos.length;
+  document.querySelector("#num-carnivores").innerText = countCarnivores();
+  document.querySelector("#num-herbivores").innerText = countHerbivores();
+  document.querySelector("#num-omnivores").innerText = countOmnivores();
+  document.querySelector("#avg-age").innerText = getAverageAge();
+}
+
+function countCarnivores() {
+  return dinos.reduce((accumulator, dino) => {
+    if (dino.isMeatEater && !dino.isPlantEater) {
+      return accumulator + 1;
+    } else {
+      return accumulator;
+    }
+  }, 0);
+}
+
+function countHerbivores() {
+  return dinos.reduce((accumulator, dino) => {
+    if (dino.isPlantEater && !dino.isMeatEater) {
+      return accumulator + 1;
+    } else {
+      return accumulator;
+    }
+  }, 0);
+}
+
+function countOmnivores() {
+  return dinos.reduce((accumulator, dino) => {
+    if (dino.isMeatEater && dino.isPlantEater) {
+      return accumulator + 1;
+    } else {
+      return accumulator;
+    }
+  }, 0);
+}
+
+function getAverageAge() {
+  let totalAge = dinos.reduce((accumulator, dino) => {
+    return accumulator + getDinoAge(dino.birthYear);
+  }, 0);
+
+  return totalAge / dinos.length;
+}
+
+// add ability to remove dino
+tableBodyElement.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove")) {
+    let dinoID = event.target.dataset.dinoId;
+    dinos.splice(dinoID, 1);
+    renderTable();
+    renderStats();
+  }
+});
