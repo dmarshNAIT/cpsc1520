fadba30b62b2412791c0d7532f2705f767e27ca6
Dana Marsh <dmarsh@nait.ca>
2024-03-11 10:36:55 -0600

    added promises example

---
+++ Winter 2024 - Section 2/promises.js
@@ -0,0 +1,41 @@
+const posts = [
+    {title: 'Post One', body:'This is post one'},
+    {title: 'Post Two', body: 'This is post two'}
+  ];
+  
+  function createPost(post) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        posts.push(post);
+  
+        const error = false;
+        //const error = true;
+  
+        if(error === false) {
+          resolve('hi there');
+        } else {
+          reject('Error: Something went wrong');
+        }
+      }, 3000);
+    });
+  }
+  
+  function outputPosts() {
+      let output = '';
+      posts.forEach(post => {
+        output += `<li>${post.title}</li>`;
+      });
+      document.body.innerHTML = output;
+  }
+  
+  outputPosts();
+  createPost({title: 'Post Three', body: 'This is post three'})
+  .then(data => {
+    console.log(data);
+    outputPosts();
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  
+  
