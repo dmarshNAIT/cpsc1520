c5edf37b6736eb81ac0d99c52b3fe010c77f0448
Dana Marsh <dmarsh@nait.ca>
2024-03-26 16:55:14 -0600

    Mar 26 class examples

--- Winter 2024 - Section 2/dom-practice-to-do/js/main.js
+++ Winter 2024 - Section 2/dom-practice-to-do/js/main.js
@@ -7,6 +7,7 @@ formElement.addEventListener('submit', (event) => {
     // call addItem(newThing)
     let newFood = formElement.querySelector('#food-input').value.trim();
     addItem(newFood);
+    formElement.reset();
 });
 
 const listElement = document.querySelector('ul.list');
@@ -27,6 +28,29 @@ function addItem(newItem) {
     // add everything to the page
     newLI.appendChild(newButton);
     listElement.appendChild(newLI);
+
+    // v2: add upvote & downvote buttons
+    // create a span, which will include a text node with a starting value of 1
+    const newSpan = document.createElement('span');
+    const spanText = document.createTextNode('1');
+    newSpan.appendChild(spanText);
+
+    // create a upvote button, which will include text & the 'upvote' class
+    const upvoteButton = document.createElement('button');
+    const upvoteText = document.createTextNode('upvote');
+    upvoteButton.classList.add('upvote');
+    upvoteButton.appendChild(upvoteText);
+
+    // create a downvote button, which will include text & the 'downvote' class
+    const downvoteButton = document.createElement('button');
+    const downvoteText = document.createTextNode('downvote');
+    downvoteButton.classList.add('downvote');
+    downvoteButton.appendChild(downvoteText);
+
+    // add the span and the 2 new buttons to our LI
+    newLI.appendChild(newSpan);
+    newLI.appendChild(upvoteButton);
+    newLI.appendChild(downvoteButton);
 }
 
 // add an event handler to the list
@@ -36,13 +60,24 @@ listElement.addEventListener('click', (event) => {
         // remove the event.target's parent
         event.target.parentElement.remove();
     }
+    // otherwise, if it's the upvote button, we will +1 to our span
+    else if(event.target.classList.contains('upvote')) {
+        let span = event.target.previousElementSibling;
+        let num = Number(span.textContent);
+        span.textContent = num + 1;
+    }
+    // otherwise, if it's the downvote button, we will -1 from our span
+    else if (event.target.classList.contains('downvote')) {
+        let span = event.target
+            .previousElementSibling.previousElementSibling;
+        let num = Number(span.textContent);
+        span.textContent = num - 1;
+    }
 });
 
 
 // TO DO : finishing touches: focus, reset, etc?
 
-// v2: add upvote, downvote buttons
 
-// v3: add to our event handler such that the vote count gets updated
 
 // challenge v4: as items are up or downvoted, move them up or down in the list
