a552f2de79d98f15ee6c5003b5f8328dd2c33299
Dana Marsh <dmarsh@nait.ca>
2024-04-02 16:38:34 -0600

    Apr 2 class examples

---
+++ Winter 2024 - Section 2/readit-fetch-npm-02/readit-fetch-npm-START/js/api.js
@@ -0,0 +1,11 @@
+function getAllPosts() {
+    // fetch from that back end server
+    // http://localhost:3000/posts
+
+    // extract the json 
+
+    // return the contents
+}
+
+
+export { getAllPosts }
---
+++ Winter 2024 - Section 2/readit-fetch-npm-02/readit-fetch-npm-START/js/index.js
@@ -0,0 +1,8 @@
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import main.js
+import './main.js';
+
+// import CSS
+import '../css/main.css';
