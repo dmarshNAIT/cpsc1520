8491115180ecc79bf2eaaae3b95ef2af1cfd33ad
Dana Marsh <dmarsh@nait.ca>
2024-04-04 10:49:27 -0600

    Apr 4 class examples

--- Winter 2024 - Section 1/npm-fundamentals-01/js/index.js
+++ Winter 2024 - Section 1/npm-fundamentals-01/js/index.js
@@ -1,9 +1,13 @@
 // this is our main file because we've imported it in our html.
 
 // import CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/main.css';
 
-
 // import some functions from 2 new .js files
+import {renderFoodTimesChart} from './food-times-chart.js'
+import {renderFoodThoughtsChart} from './food-thoughts-chart.js'
 
 // call those new functions
+renderFoodTimesChart();
+renderFoodThoughtsChart();
