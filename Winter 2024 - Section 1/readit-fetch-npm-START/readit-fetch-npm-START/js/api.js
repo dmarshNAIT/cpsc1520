0ec0ae88888fbf5a0e671aea4d805771507074db
Dana Marsh <dmarsh@nait.ca>
2024-04-04 16:59:17 -0600

    Apr 4 class examples

---
+++ Winter 2024 - Section 1/readit-fetch-npm-START/readit-fetch-npm-START/js/api.js
@@ -0,0 +1,8 @@
+// where we do our API calls
+
+// create a function called getAllPosts()
+// this will fetch data from the backend server
+// http://localhost:3000/posts
+// return the contents of the json
+
+// export getAllPosts
---
+++ Winter 2024 - Section 1/readit-fetch-npm-START/readit-fetch-npm-START/js/index.js
@@ -0,0 +1,9 @@
+// import styles:
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import our own CSS
+import '../css/main.css'
+
+// import the entire main.js
+import './main.js'
