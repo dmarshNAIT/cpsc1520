8dddfa3b726a84aea88ea1bbb3bf1118faf4b6a1
Dana <dmarsh@nait.ca>
2024-07-15 11:36:38 -0600

    clean up unnecessary files

--- class-second-example-START/class-second-example-START/README.md
+++
@@ -1,5 +0,0 @@
-# Class Balloon Example
-
-Initialize this project with "npm install" in this folder.
-
-Run this project with NPM run start.
--- readit-fetch-npm-START/js/index.js
+++
@@ -1,8 +0,0 @@
-// import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-// import CSS
-import '../css/main.css'
-
-// import main.js
-import './main.js'
