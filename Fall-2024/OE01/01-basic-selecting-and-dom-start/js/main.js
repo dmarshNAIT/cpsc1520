903c6119c7bcd28d2a64ea9140d222a8e0844bef
Dana <dmarsh@nait.ca>
2024-09-04 21:37:56 -0600

    posted Sep 4 class examples

---
+++ Fall-2024/OE01/01-basic-selecting-and-dom-start/js/main.js
@@ -0,0 +1,15 @@
+// this is a comment: it is just for humans
+
+// print a message to confirm that my script is linked:
+console.log('is this working?');
+
+// let's do some formatting
+let colourElement = document.querySelector('#update-colour p');
+colourElement.style.color = 'blue';
+
+// for practice:
+// try changing the text of the 2nd paragraph to something else
+// try changing the colour of the 1st paragraph to something else
+
+// stretch challenge:
+// add a <strong> tag to the 2nd paragraph using innerHTML
