08b4b0a067a515b4b3ae17920d17f9d28b3bd310
Dana Marsh <dmarsh@nait.ca>
2025-01-10 09:50:37 -0700

    Add first JS example

---
+++ Winter2025-SectionA04/01-basic-selecting-and-dom-end/js/main.js
@@ -0,0 +1,8 @@
+// make sure I linked this correctly
+console.log('linked!');
+
+// fix the typo 
+let typoElement = document.querySelector('#update-text p');
+typoElement.innerText = 'Correct the typos in this text.';
+
+// create the rest of my variables for the other 2 paragraphs
