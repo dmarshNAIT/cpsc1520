54517d92d806ed88587065db6042cd31537e550f
Dana Marsh <dmarsh@nait.ca>
2025-01-10 14:51:07 -0700

    Add Day 2 examples 🍌

---
+++ Winter2025-SectionA03/01-basic-selecting-and-dom-start/js/main.js
@@ -0,0 +1,9 @@
+console.log('linked!');
+
+// let's fix the typo:
+let typoElement = document.querySelector('#update-text p');
+typoElement.innerText = 'Correct the typos in this text.';
+
+
+
+// create variables for the remaining paragraphs
