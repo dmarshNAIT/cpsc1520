5f9688dcaf48f31b9e45400f98162c2909c9ea7e
Dana Marsh <dmarsh@nait.ca>
2025-01-13 12:26:12 -0700

    Finish Week 1 example

--- Winter2025-SectionA03/01-basic-selecting-and-dom-start/js/main.js
+++ Winter2025-SectionA03/01-basic-selecting-and-dom-start/js/main.js
@@ -4,6 +4,14 @@ console.log('linked!');
 let typoElement = document.querySelector('#update-text p');
 typoElement.innerText = 'Correct the typos in this text.';
 
+// let's now add <strong> tags to the next paragraph
+let strongElement = document.querySelector('#update-html p');
+strongElement.innerHTML = '<strong>' + strongElement.innerText + '</strong>';
 
+// now let's change the last element to blue
+let blueElement = document.querySelector('#update-colour p');
+blueElement.style.color = 'blue';
 
-// create variables for the remaining paragraphs
+// TWO CHALLENGES:
+// "Update text" --> make that your favourite colour
+// "Update HTML" --> make it italicized
