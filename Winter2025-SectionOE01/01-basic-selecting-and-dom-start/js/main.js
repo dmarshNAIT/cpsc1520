4f2c6da05c6cfc6c83db14b9445b62e470722bd9
Dana <dmarsh@nait.ca>
2025-01-15 21:38:12 -0700

    Finish Week 1 example 👏

--- Winter2025-SectionOE01/01-basic-selecting-and-dom-start/js/main.js
+++ Winter2025-SectionOE01/01-basic-selecting-and-dom-start/js/main.js
@@ -1,7 +1,19 @@
 // let's make sure our JS file is correctly linked:
 console.log('linked!');
 
+
 // first, we will target the element with typos:
 let typoElement = document.querySelector('#update-text p');
 // now let's change the contents to a new message:
 typoElement.innerText = 'Correct the typos in this text.';
+
+// next, let's target the 2nd paragraph:
+let strongElement = document.querySelector('#update-html p');
+// we will replace its HTML contents with a pair of strong tags around the current contents:
+strongElement.innerHTML = '<strong>' + strongElement.innerHTML + '</strong>';
+
+// let's target the last paragraph:
+let blueElement = document.querySelector('#update-colour p');
+// and make it blue:
+blueElement.style.color = 'blue';
+
