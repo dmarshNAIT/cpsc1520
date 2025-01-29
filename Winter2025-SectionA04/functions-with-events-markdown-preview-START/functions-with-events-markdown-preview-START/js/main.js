ea8db323d4a4a0572210ba907a368050f6823272
Dana Marsh <dmarsh@nait.ca>
2025-01-28 18:02:41 -0700

    Add starter code for forms

---
+++ Winter2025-SectionA04/functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
@@ -0,0 +1,21 @@
+// this is our javascript file.
+
+
+// in the html we've linked the 
+// marked js package and we can see it's available here
+// through the global scope of javascript.
+// more information see here: https://marked.js.org/
+
+
+// select the text input. 
+
+
+// make an event listener that listens to keydown on the input
+    // call the render preview function
+    // you'll see that the preview changes
+    // every time we have something.
+
+
+// create a function call renderPreview
+    // select the mark down element
+    // use the imported library called marked and parse the new input value.
---
+++ Winter2025-SectionOE01/events-forms-START/events-forms-START/js/main.js
@@ -0,0 +1,30 @@
+// this is our javascript file.
+
+// intercept the form.
+
+// focus to the first element
+
+// create an event listener on the form.
+    // get the form element values for title, description and amount
+    // prevent the form from being submitted.
+    // get the form values.
+    // add the item
+    // update current budget total
+    // reset the values
+    // focus back at the title
+
+
+// add a budget item create function
+/*
+<li class="list-group-item list-group-item-action" aria-current="true">
+    TITLE HERE (AMOUNT HERE) - DESCRIPTION HERE
+</li>
+*/
+
+// add a updateTotal function
+
+
+// add active class to list item on mouseover
+
+
+// remove active class from list item on mouseout.
