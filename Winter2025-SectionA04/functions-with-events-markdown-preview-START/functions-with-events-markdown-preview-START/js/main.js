606a8b043e5191247f101dac80bd5ad4ba4e0e56
Dana Marsh <dmarsh@nait.ca>
2025-01-31 09:36:17 -0700

    Add markdown parse examples 🐍

--- Winter2025-SectionA04/functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
+++ Winter2025-SectionA04/functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
@@ -1,5 +1,5 @@
 // this is our javascript file.
-
+console.log('hello world');
 
 // in the html we've linked the 
 // marked js package and we can see it's available here
@@ -8,14 +8,20 @@
 
 
 // select the text input. 
-
+const textInput = document.querySelector('#markdown-input-text');
 
 // make an event listener that listens to keydown on the input
     // call the render preview function
     // you'll see that the preview changes
     // every time we have something.
-
+textInput.addEventListener('keyup', (event) => {
+    console.log(event.target.value);
+    renderPreview(event.target.value);
+});
 
 // create a function call renderPreview
     // select the mark down element
     // use the imported library called marked and parse the new input value.
+const renderPreview = (newContent) => { 
+    document.querySelector('#markdown-preview').innerHTML = marked.parse(newContent);
+};
