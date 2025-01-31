f3792e77d91ccb9615561cab0b6cd935e039a587
Dana Marsh <dmarsh@nait.ca>
2025-01-31 09:41:53 -0700

    Add mini-challenge to budget forms example

--- Winter2025-SectionA04/events-forms-START/events-forms-START/js/main.js
+++ Winter2025-SectionA04/events-forms-START/events-forms-START/js/main.js
@@ -1,19 +1,22 @@
 // this is our javascript file.
 
 // intercept the form.
-
+const form = 
 // focus to the first element
 
 // create an event listener on the form.
     // get the form element values for title, description and amount
     // prevent the form from being submitted.
     // get the form values.
+    const title = // the value in the title
+    const description =
+    const amount =
     // add the item
     // update current budget total
     // reset the values
     // focus back at the title
 
-
+const budgetList = // this represents the UL element
 // add a budget item create function
 /*
 <li class="list-group-item list-group-item-action" aria-current="true">
