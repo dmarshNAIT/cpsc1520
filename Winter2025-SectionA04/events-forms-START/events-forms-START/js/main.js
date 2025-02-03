31857f7c55ac315d7ef7a1a0ef24eb88e8101cbc
Dana Marsh <dmarsh@nait.ca>
2025-02-03 09:29:13 -0700

    Add variable declarations

--- Winter2025-SectionA04/events-forms-START/events-forms-START/js/main.js
+++ Winter2025-SectionA04/events-forms-START/events-forms-START/js/main.js
@@ -1,22 +1,22 @@
 // this is our javascript file.
 
 // intercept the form.
-const form = 
+const form = document.querySelector('#budget-form');
 // focus to the first element
 
 // create an event listener on the form.
     // get the form element values for title, description and amount
     // prevent the form from being submitted.
     // get the form values.
-    const title = // the value in the title
-    const description =
-    const amount =
+    const title = document.querySelector('input[name=budget-title]');    // the value in the title
+    const description = document.querySelector('#budget-item-description').value;
+    const amount = document.querySelector('#budget-item-amount').value;
     // add the item
     // update current budget total
     // reset the values
     // focus back at the title
 
-const budgetList = // this represents the UL element
+const budgetList = document.querySelector('.current-budget'); // this represents the UL element
 // add a budget item create function
 /*
 <li class="list-group-item list-group-item-action" aria-current="true">
