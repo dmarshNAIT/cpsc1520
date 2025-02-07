61cfbac5bea601e03195223916ebce52ba6eec7e
Dana Marsh <dmarsh@nait.ca>
2025-02-07 09:15:36 -0700

    Add validation to budget form example

--- Winter2025-SectionA04/events-forms-START/events-forms-START/js/main.js
+++ Winter2025-SectionA04/events-forms-START/events-forms-START/js/main.js
@@ -2,27 +2,67 @@
 
 // intercept the form.
 const form = document.querySelector('#budget-form');
+// create references to the form elements:
+const titleElement = document.querySelector('input[name=budget-title]');
+const descriptionElement = document.querySelector('#budget-item-description');
+const amountElement = document.querySelector('#budget-item-amount');
+
 // focus to the first element
-document.querySelector('input[name=budget-title]').focus();
+titleElement.focus();
 
 // create an event listener on the form.
 form.addEventListener('submit', (event) => {
-    // get the form element values for title, description and amount
     // prevent the form from being submitted.
     event.preventDefault();
     // get the form values.
-    const title = document.querySelector('input[name=budget-title]').value;   
-    const description = document.querySelector('#budget-item-description').value;
-    const amount = document.querySelector('#budget-item-amount').value;
+    const title = titleElement.value;
+    const description = descriptionElement.value;
+    const amount = amountElement.value;
 
-    // add the item
-    addBudgetItem(title, amount, description);
-    // update current budget total
-    updateTotal(amount);
-    // reset the values
-    form.reset();
-    // focus back at the title
-    document.querySelector('input[name=budget-title]').focus();
+    let isFormValid = true;
+
+    // VALIDATE EACH FORM ELEMENT
+    // validate title
+    if (isTextEmpty(title)) {    // title is empty
+        // it's invalid! mark it as invalid, mark form as invalid
+        titleElement.classList.add('is-invalid');
+        isFormValid = false;
+    } else {   // otherwise, it's valid! remove the invalid marker
+        titleElement.classList.remove('is-invalid');
+    }
+
+    // validate description
+    if (isTextEmpty(description)) {    // description is empty
+        // it's invalid! mark it as invalid, mark form as invalid
+        descriptionElement.classList.add('is-invalid');
+        isFormValid = false;
+    } else {   // otherwise, it's valid! remove the invalid marker
+        descriptionElement.classList.remove('is-invalid');
+    }
+
+    // validate amount
+    // check if amount is greater than zero
+    // if so: valid! remove the invalid marker
+    // otherwise: invalid! mark it as invalid, mark form as invalid
+    if (isGreaterThanZero(amount)) {
+        amountElement.classList.remove('is-invalid');
+    }
+    else {
+        amountElement.classList.add('is-invalid');
+        isFormValid = false;
+    }
+
+    // IF THE FORM IS VALID:
+    if (isFormValid) {
+        // add the item
+        addBudgetItem(title, amount, description);
+        // update current budget total
+        updateTotal(amount);
+        // reset the values
+        form.reset();
+        // focus back at the title
+        titleElement.focus();
+    }
 });
 
 const budgetList = document.querySelector('.current-budget'); // this represents the UL element
@@ -47,7 +87,7 @@ const updateTotal = (amount) => {
     const totalElement = document.querySelector('#budget-total');
 
     // add the new amount to the total & update the HTML
-    totalElement.innerText =  parseInt(totalElement.innerText) + parseInt(amount);
+    totalElement.innerText = parseInt(totalElement.innerText) + parseInt(amount);
 };
 
 // add active class to list item on mouseover
@@ -59,3 +99,12 @@ budgetList.addEventListener('mouseover', (event) => {
 budgetList.addEventListener('mouseout', (event) => {
     event.target.classList.remove('active');
 });
+
+/* VALIDATOR FUNCTIONS */
+const isTextEmpty = (value) => {
+    return (value.length == 0);
+}
+
+const isGreaterThanZero = (value) => {
+    return (parseInt(value) > 0);
+};
