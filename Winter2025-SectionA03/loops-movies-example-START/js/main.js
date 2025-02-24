de89d54770fe180407963c33ba03e5df7231b82f
Dana Marsh <dmarsh@nait.ca>
2025-02-24 10:33:45 -0700

    Add starter code

---
+++ Winter2025-SectionA03/loops-movies-example-START/js/main.js
@@ -0,0 +1,21 @@
+// this is our javascript file
+
+// intercept the form and/or required elements
+
+// focus on the filter element
+
+// select all movie list items the descendant css selector
+// .top-movies-list li
+
+// get the form value and call the function filterItems
+
+/*
+
+We're going to create a function named "filterItems"
+that check each item contains our search query
+- if it's not, then we're going to add the "hidden-item" class
+- if does include it, we're going to remove the "hidden-item" class
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+
+*/
