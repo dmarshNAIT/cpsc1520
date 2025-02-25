d96c75bd5011f4c15504179555cb0df21d142202
Dana <dmarsh@nait.ca>
2025-02-24 21:02:23 -0700

    Add movie example 🎞️

--- Winter2025-SectionOE01/loops-movies-example-START/js/main.js
+++ Winter2025-SectionOE01/loops-movies-example-START/js/main.js
@@ -1,15 +1,24 @@
-// this is our javascript file
+(function () {
+  // this is our javascript file
+  console.log('yay movies!');
 
-// intercept the form and/or required elements
+  // intercept the form and/or required elements
+  const form = document.querySelector('#movie-filter-form');
+  const searchBox = document.querySelector('.form-control');
 
-// focus on the filter element
+  // focus on the filter element
+  searchBox.focus();
 
-// select all movie list items the descendant css selector
-// .top-movies-list li
+  // select all movie list items the descendant css selector
+  // .top-movies-list li
+  const movieListItems = document.querySelectorAll('.top-movies-list li');
 
-// get the form value and call the function filterItems
-
-/*
+  // get the form value and call the function filterItems
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    filterItems(searchBox.value);
+  });
+  /*
 
 We're going to create a function named "filterItems"
 that check each item contains our search query
@@ -19,3 +28,19 @@ that check each item contains our search query
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 
 */
+  const filterItems = (searchTerm) => {
+    movieListItems.forEach((movieItem, index, movieList) => {
+      // for each movie item, check if it contains the search term
+      if (
+        movieItem.innerText.toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        // if so, unhide it
+        movieItem.classList.remove('hidden-item');
+      } else {
+        // otherwise, hide it
+        movieItem.classList.add('hidden-item');
+      }
+    });
+  };
+  
+})(); // immediately-invoked function expression
