8fc67e99d3ace34175a467f858ee3d235aeacde9
Dana <dmarsh@nait.ca>
2025-02-26 09:45:46 -0700

    Finish AddTask and RenderTasks ✅

--- Winter2025-SectionA03/07-intro-to-object-literals-start/js/main.js
+++ Winter2025-SectionA03/07-intro-to-object-literals-start/js/main.js
@@ -18,15 +18,14 @@
 */
 
 let todos = [
-    {
-        description: 'Complete this example',
-        complete: false,
-
-    },
-    {
-        description: 'Learn about objects',
-        complete: true,
-    }
+  {
+    description: 'Complete this example',
+    complete: false,
+  },
+  {
+    description: 'Learn about objects',
+    complete: true,
+  },
 ];
 
 // TODO #1: let's add an event listener that waits for us to submit the form
@@ -37,10 +36,52 @@ let todos = [
 
 let form = document.getElementById('add-todo-form');
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let taskDescription = form.elements['todo-description'].value;
-    addTask(taskDescription);
+  event.preventDefault();
+  let taskDescription = form.elements['todo-description'].value;
+  addTask(taskDescription);
 });
+
+const addTask = (newTaskDescription) => {
+  // create a new object to represent this new task
+  const newTask = {
+    description: newTaskDescription,
+    complete: false,
+  };
+  //console.log(newTask);
+
+  // add the task to the array
+  todos.push(newTask);
+  //console.log(todos);
+
+  // call renderTasks
+  renderTasks();
+};
+
+const renderTasks = () => {
+  // HIGH LEVEL PLAN:
+  // iterate through the elements in the array
+  // and add each to the HTML content
+
+  // PSEUDO-CODE:
+  // for each element in the array
+  // create a <li>
+  // then add that <li> to the <ul>
+
+  todos.forEach((task, index, todoList) => {
+    const newLi =   `<li class="list-group-item">
+                        <input class="form-check-input todo-status"
+                            data-todo-id="${index}"
+                            type="checkbox"
+                            value="todo-${index}"
+                            ${task.complete ? 'checked' : ''}>
+                        ${task.description}
+                    </li>`;
+
+    const ul = document.querySelector('.list-group');
+
+    ul.innerHTML = ul.innerHTML + newLi;
+  });
+};
 
 // TODO #2: we'll add another event listener that waits for changes to the checkboxes (e.g. complete)
 // it will call a helper function called calculateCount which will count the # of completed todos
