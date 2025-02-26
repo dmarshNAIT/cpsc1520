f2561e6ece4609adf558bd4c4a04e2be63701f44
Dana <dmarsh@nait.ca>
2025-02-26 12:21:58 -0700

    Add starter files

---
+++ Winter2025-SectionOE01/07-intro-to-object-literals-start/js/main.js
@@ -0,0 +1,31 @@
+/*
+    Todolist
+
+    We're going to make a todolist that counts how many todos we've completed.
+    We'll do this using objects in the todos array shown below.
+
+    HTML FOR The todo
+
+    <li class="list-group-item">
+        <input class="form-check-input todo-status"
+            data-todo-id="INDEX HERE"
+            type="checkbox"
+            value="todo-INDEX HERE"
+            COMPLETE HERE>
+        DESCRIPTION HERE
+    </li>
+
+*/
+
+let todos = [
+    {
+        description: 'Todo 1',
+        complete: false,
+
+    },
+    {
+        description: 'Todo 2',
+        complete: true,
+    }
+];
+
