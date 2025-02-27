24c80e2bbb80a813ec3bda100839546436fc2962
Dana <dmarsh@nait.ca>
2025-02-26 19:58:44 -0700

    Add event listener to add new tasks ✅

--- Winter2025-SectionOE01/07-intro-to-object-literals-start/js/main.js
+++ Winter2025-SectionOE01/07-intro-to-object-literals-start/js/main.js
@@ -18,14 +18,62 @@
 */
 
 let todos = [
-    {
-        description: 'Todo 1',
-        complete: false,
-
-    },
-    {
-        description: 'Todo 2',
-        complete: true,
-    }
+  {
+    description: 'Todo 1',
+    complete: false,
+  },
+  {
+    description: 'Todo 2',
+    complete: true,
+  },
 ];
 
+// TODO STEP 1:
+// HIGH LEVEL PLAN:
+// create the event listener that adds tasks to the page
+// when we click submit: we take the inputted text and pass it to another function called addTask
+// addTask: + the task to the array, & then call renderTasks
+// renderTasks: + the task to the HTML
+
+const form = document.getElementById('add-todo-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = form.elements['todo-description'].value;
+  addTask(taskDescription);
+});
+
+const addTask = (newTaskDescription) => {
+  // create a new object
+  const newTask = {
+    description: newTaskDescription,
+    complete: false,
+  };
+
+  // add the object to the array
+  todos.push(newTask);
+
+  renderTasks();
+};
+
+const renderTasks = () => {
+  const ul = document.querySelector('.todo-list');
+
+  todos.forEach((task, index, taskArray) => {
+    // create a new li
+    const newLi = `<li class="list-group-item">
+        <input class="form-check-input todo-status"
+            data-todo-id="${index}"
+            type="checkbox"
+            value="todo-${index}"
+            ${task.complete ? 'checked' : ''}>
+        ${task.description}
+    </li>`;
+
+    // add the li to ul
+    ul.innerHTML = ul.innerHTML + newLi;
+  });
+};
+
+// TODO STEP 2:
+// create another event listener that updates the count when we CHANGE the status of a task
