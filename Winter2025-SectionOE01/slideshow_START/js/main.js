91dc85843d93f9afbe35a4e0e5fd9485d9a6ef2a
Dana <dmarsh@nait.ca>
2025-03-05 21:23:03 -0700

    Finish event listener

--- Winter2025-SectionOE01/slideshow_START/js/main.js
+++ Winter2025-SectionOE01/slideshow_START/js/main.js
@@ -2,15 +2,31 @@
 let images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
 let currentImg = 0;
 let idx;
+const imageTracker = document.querySelector('.image-tracker');
 
 // display the current image
 document.querySelector('.carousel>img').src = 'images/' + images[0]; 
 
 // add the appropriate number of selector bullets
-// TODO: 
+// for each element in the array, add <span>
+// <span class="control" data-idx="INDEX">&bull;</span>
+
+// v1: using forEach
+// for(let index = 0; index < images.length; index++) {
+//     imageTracker.innerHTML += `<span class="control" data-idx="${index}">&bull;</span>`;
+// };
+
+// v2: using for/of
+let index = 0;
+for(let image of images) {
+    imageTracker.innerHTML += `<span class="control" data-idx="${index}">&bull;</span>`;
+    index++;
+};
 
 // highlight the first bullet as 'active'
-// TODO:
+// instead of document.querySelector, we can query inside of ANY html element:
+let activeBullet = imageTracker.querySelector('span.control');
+activeBullet.classList.add('active');
 
 // add event listener for carousel controls
 document.querySelector('.carousel').addEventListener('click', function (evt){
@@ -19,21 +35,35 @@ document.querySelector('.carousel').addEventListener('click', function (evt){
         if (target.classList.contains('next')) {
             // move to the next index in the array
             currentImg += 1;
+            if (currentImg == images.length) {
+                currentImg = 0;
+            }
+
         } else if (target.classList.contains('prev')){
             // move to the previous index in the array
             currentImg -= 1;
+            // we don't want to go into negative indexes; if that happens, correct:
+            if (currentImg === -1) {
+                currentImg = images.length - 1;
+            }
+
+        } else {
+            // selector bullet clicked
+            currentImg = Number(target.dataset.idx);
         }
-		// selector bullet clicked
-    	// TODO:
-		
+		//console.log("the current image # is", currentImg);
+
         // display the new current image
         document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
 
         // update the active selector bullet
-        // TODO:
-
+        // remove the active class
+        imageTracker.querySelector('span.active').classList.remove('active');
+        // add it to the new active element
+        imageTracker.querySelectorAll('span')[currentImg].classList.add('active');
     }
 }); 
 
+// TODO: what happens if we scroll too far?
 
 // TODO: add another event listener
