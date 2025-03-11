9b64750b831a7362dbf0ed9da1270464915cbefe
Dana <dmarsh@nait.ca>
2025-03-10 21:06:25 -0600

    Complete dog example

--- Winter2025-SectionOE01/fetch-fundamentals-dogs-START/js/main.js
+++ Winter2025-SectionOE01/fetch-fundamentals-dogs-START/js/main.js
@@ -10,3 +10,32 @@ In this example we're going to generate random pictures of dogs.
 4. Call the get random dog function in your event listener,
     and call it when the page loads.
 */
+
+const button = document.getElementById('new-dog-button');
+
+button.addEventListener('click', (params) => {
+    getRandomDog(renderDog);
+});
+
+const getRandomDog = (callback) => {
+    // fetch --> then --> then
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then( (response) => response.json())
+    .then( (dogData) => { callback(dogData)});
+}
+
+const logDogInfo = (dogInfo) => {
+    // print dogInfo to the console
+    console.log(dogInfo);
+}
+
+const renderDog = (dogInfo) => {
+    // setting a value for src attribute
+    const dogImage = document.querySelector('.dog-image');
+   // v1: modify the src attribute of the HTML element:
+    // dogImage.src = dogInfo.message;
+    // v2: using setAttribute()
+    dogImage.setAttribute('src', dogInfo.message);
+}
+
+getRandomDog(renderDog);
