92344d2d368b5d8ff34b8d793d4a5e06122e73b0
Dana <dmarsh@nait.ca>
2025-03-10 20:31:03 -0600

    Add function to render cats on screen 😽

--- Winter2025-SectionOE01/fetch-local-fundamentals-cats-START/js/main.js
+++ Winter2025-SectionOE01/fetch-local-fundamentals-cats-START/js/main.js
@@ -15,16 +15,51 @@ Sample HTML
     </div>
 
 Use the following list as a guide to complete the task:
-
-- select any requried elements from the document
-- create a getCats function that will fetch the local cats-with-jobs.json file
-    - the function should accept a function parameter, that will be called 
-      when the data has been resolved, and passed the array of fetched cats
-      as a single argument
-- create a renderCats function that will accept a single parameter, which
-  should be an array of cat data
-    - the function should iterate over the array (e.g. use .forEach()) and render
-      each cat using the template HTML provided above
-- call the getCats function, and pass in the renderCats function as an argument
-    - when the page loads, you should now see the cats rendered on the page
 */
+
+// - select any requried elements from the document
+
+// - create a getCats function that will fetch the local cats-with-jobs.json file
+//     - the function should accept a function parameter, that will be called
+//       when the data has been resolved, and passed the array of fetched cats
+//       as a single argument
+
+const getCats = (callbackFunction) => {
+  fetch('./data/cats-with-jobs.json')
+    // v1: the "original" syntax:
+    // .then((response) => {
+    //   return response.json();
+    // })
+    // v2: the shorter syntax:
+    .then((response) => response.json())
+    .then((catData) => {
+      callbackFunction(catData);
+    });
+};
+
+// test:
+getCats(console.log);
+
+// - create a renderCats function that will accept a single parameter, which
+//   should be an array of cat data
+//     - the function should iterate over the array (e.g. use .forEach()) and render
+//       each cat using the template HTML provided above
+const renderCats = (catData) => {
+  const catDisplay = document.querySelector('.cat-display');
+  
+  catDisplay.innerHTML = '';
+
+  for(let cat of catData) {
+    catDisplay.innerHTML += `    <div class="mt-3 card" >
+        <img class="card-img-top" src="${cat.picture}" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">${cat.name}</h5>
+        <p class="card-text">${cat.text}</p>
+        </div>
+    </div>`;
+  }
+}
+
+// - call the getCats function, and pass in the renderCats function as an argument
+//     - when the page loads, you should now see the cats rendered on the page
+getCats(renderCats);
