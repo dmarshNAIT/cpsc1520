0f9ae63f9a1accf5a13e6827c5b795714ad41cb1
Dana <dmarsh@nait.ca>
2025-03-10 19:56:17 -0600

    Add fetch demo

--- Winter2025-SectionOE01/fetch-demo/js/main.js
+++ Winter2025-SectionOE01/fetch-demo/js/main.js
@@ -1,32 +1,34 @@
 // fetch plain text
 fetch('data/lorem.txt')
-    .then((res) => {
+    .then((response) => {
+        return response.text();
         // if (res.ok) {
-            console.log(res)
         // return res.text();
         // } else {
         //     throw Error(`Error ${res.status}`);
         // }
     })
-    // .then((text) => {
-    //     document.querySelector('.text-output').innerText = text;
-    // });
+    .then((text) => {
+         document.querySelector('.text-output').innerText = text;
+    });
 
 // fetch json data
-fetch('data/people.json')
-    .then((res) => {
+fetch('./data/people.json')
+    .then((response) => {
+        // console.log(response);
+        return response.json();
         // if (res.ok) {
-            console.log(res)
         // return res.json();
         // } else {
         //     throw Error(`Error ${res.status}`);
         // }
     })
-    // .then((data) => {
-    //     // TODO: actually render the properties of the objects
-    //     data.forEach(person => {
-    //         document.querySelector('.json-output').innerHTML += `
-    //             <p>${person.firstName}</p>
-    //         `
-    //     });
-    // });
+    .then((data) => {
+         //console.log(data)
+        // // TODO: actually render the properties of the objects
+        data.forEach(person => {
+            document.querySelector('.json-output').innerHTML += `
+                <p>${person.firstName}</p>
+            `
+        });
+    });
