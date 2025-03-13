cf118f49e415eaaad6854e6ce438211676b0907d
Dana Marsh <dmarsh@nait.ca>
2025-03-13 13:37:53 -0600

    Add fetch demo

--- Winter2025-SectionA04/fetch-demo/js/main.js
+++ Winter2025-SectionA04/fetch-demo/js/main.js
@@ -1,27 +1,34 @@
 // fetch plain text
-fetch('/data/lorem.txt')
+fetch('./data/lorem.txt')
     .then((res) => {
-        if (res.ok) {
+        console.log(res);
+
+        // if (res.ok) {
+        // return res.text();
+        // } else {
+        //     throw Error(`Error ${res.status}`);
+        // }
         return res.text();
-        } else {
-            throw Error(`Error ${res.status}`);
-        }
     })
     .then((text) => {
+        console.log(text);
         document.querySelector('.text-output').innerText = text;
     });
 
 // fetch json data
-fetch('/data/people.json')
+fetch('./data/people.json')
     .then((res) => {
-        if (res.ok) {
+        console.log(res);
+        // if (res.ok) {
+        // return res.json();
+        // } else {
+        //     throw Error(`Error ${res.status}`);
+        // }
         return res.json();
-        } else {
-            throw Error(`Error ${res.status}`);
-        }
     })
     .then((data) => {
-        // TODO: actually render the properties of the objects
+        console.log(data);
+        // actually render the properties of the objects
         data.forEach(person => {
             document.querySelector('.json-output').innerHTML += `
                 <p>${person.firstName}</p>
