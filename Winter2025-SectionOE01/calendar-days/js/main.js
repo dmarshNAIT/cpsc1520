b27f8e6b0b2e73eb7efb8eb303dea0f3f7769e50
Dana <dmarsh@nait.ca>
2025-03-17 20:26:43 -0600

    Revise starter code for calendar

--- Winter2025-SectionOE01/calendar-days/js/main.js
+++ Winter2025-SectionOE01/calendar-days/js/main.js
@@ -9,6 +9,9 @@ let days = [
     'Sat'
 ];
 
+// TODO: create variables to represent the year & current month
+// TODO: create variables to represent the first and last days of the month
+
 // variable for the table, which includes a row for day names
 let cal = '<table border="1"><tr>';
 
@@ -18,33 +21,16 @@ for (let idx = 0; idx < days.length; idx += 1) {
 }
 
 // close the table; ready for display
-cal += '</tr></table>';
+cal += '</tr>';
+
+// TODO: start a new row
+
+// TODO: loop through all the days in the month and add them as td
+
+// TODO: add blanks if the month doesn't start on Sunday
+
+// TODO: add new rows where needed
+
+cal += '</table>';
 
 document.querySelector('.calendar').innerHTML = cal;
-
-
-
-// TODO part 1: create a function called getCalendar that takes 2 params: year & month
-// create variables to represent the first & last days of the month
-
-// create the heading
-
-// open the table tag
-// open the row tag
-
-// print out the names of the days as <th> elements
-
-// end that row & start another
-
-// now, loop through the days of the month, from 0 to the last day.
-// + blank elements for "empty" days e.g. if the month doesn't start on Sunday
-// + a <td> for the current day
-// close a row if needed
-// start a new row if needed
-
- // return the completed calendar
-
-
-
-
-// TODO part 2:  call getCalendar with today's date & use it to update the webpage
