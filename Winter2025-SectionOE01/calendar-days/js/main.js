0399de1a3b530ceda50d8ee51810479f75a729cc
Dana <dmarsh@nait.ca>
2025-03-17 20:51:23 -0600

    Add first draft of calendar

--- Winter2025-SectionOE01/calendar-days/js/main.js
+++ Winter2025-SectionOE01/calendar-days/js/main.js
@@ -1,35 +1,40 @@
 // array of day names
-let days = [
-    'Sun',
-    'Mon',
-    'Tue',
-    'Wed',
-    'Thu',
-    'Fri',
-    'Sat'
-];
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-// TODO: create variables to represent the year & current month
-// TODO: create variables to represent the first and last days of the month
+// create variables to represent the year & current month
+const year = 2025; // TODO: make this automatic
+const month = 2; // TODO: make this automatic
+// create variables to represent the first and last days of the month
+const firstDay = new Date(year, month, 1);
+const lastDay = new Date(year, month + 1, 0);
 
 // variable for the table, which includes a row for day names
 let cal = '<table border="1"><tr>';
 
 for (let idx = 0; idx < days.length; idx += 1) {
-    // add each day name to the row
-    cal += '<th>' + days[idx] + '</th>';
+  // add each day name to the row
+  cal += '<th>' + days[idx] + '</th>';
 }
 
-// close the table; ready for display
-cal += '</tr>';
+// end the header row and start another:
+cal += '</tr><tr>';
 
-// TODO: start a new row
+// add blanks if the month doesn't start on Sunday
+for (let blanks = 0; blanks < firstDay.getDay(); blanks++) {
+  cal += '<td></td>';
+}
 
-// TODO: loop through all the days in the month and add them as td
+for (let counter = 0; counter < lastDay.getDate(); counter++) {
+  cal += '<td>' + (counter + 1) + '</td>';
 
-// TODO: add blanks if the month doesn't start on Sunday
+  // TODO: add new rows where needed
+  if () { // it's the end of the week
+    // close the current tr
 
-// TODO: add new rows where needed
+    // if it's NOT the end of the month
+    // start a new tr
+  }
+}
 
 cal += '</table>';
 
