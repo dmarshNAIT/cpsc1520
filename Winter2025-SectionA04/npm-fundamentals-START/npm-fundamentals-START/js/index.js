4972f29389ba107c70136f81d23470ea034774c8
Dana Marsh <dmarsh@nait.ca>
2025-03-31 14:00:57 -0600

    Finish npm fundamentals example 📊

--- Winter2025-SectionA04/npm-fundamentals-START/npm-fundamentals-START/js/index.js
+++ Winter2025-SectionA04/npm-fundamentals-START/npm-fundamentals-START/js/index.js
@@ -1,1 +1,13 @@
 // this is our main file because we've imported it in our html.
+
+// import our CSS here
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/main.css';
+
+// import those 2 chart functions
+import {renderFoodThoughtsChart} from './food-thoughts-chart.js';
+import {renderFoodTimesChart} from './food-times-chart.js';
+
+// run the chart functions
+renderFoodThoughtsChart();
+renderFoodTimesChart();
